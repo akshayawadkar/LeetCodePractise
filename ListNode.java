59e92630f5c82c7a7cc2c27dbeee7659caf5f6aa
akshayawadkar <mailaw3515@gmail.com>
2020-07-24 20:11:55 -0700

    added new

--- ListNode.java
+++ ListNode.java
@@ -3,7 +3,7 @@ package com.leetcode;
 public class ListNode {
 
     int val;
-    com.company.ListNode next;
+    ListNode next;
 
     ListNode(){
 
@@ -13,24 +13,24 @@ public class ListNode {
         this.val = val;
     }
 
-    ListNode(int val, com.company.ListNode next){
+    ListNode(int val, ListNode next){
         this.val = val;
         this.next = next;
     }
 
-    public static com.company.ListNode getList(int[] arr) {
-        com.company.ListNode head = new com.company.ListNode(-1);
-        com.company.ListNode curr = head;
+    public static ListNode getList(int[] arr) {
+        ListNode head = new ListNode(-1);
+        ListNode curr = head;
 
         for(int i : arr){
-            curr.next = new com.company.ListNode(i);
+            curr.next = new ListNode(i);
             curr = curr.next;
         }
 
         return head.next;
     }
 
-    public static void display(com.company.ListNode head){
+    public static void display(ListNode head){
 
         while(head != null){
             System.out.print(head.val+" ");
--- _206.java
+++ _206.java
@@ -1,19 +1,21 @@
 package com.leetcode;
 
+import com.leetcode.ListNode;
+
 public class _206 {
-    public com.company.ListNode reverseList(com.company.ListNode head) {
+    public ListNode reverseList(ListNode head) {
         
         if(head == null || head.next == null){
             return head;
         }
         
         
-        com.company.ListNode curr = head;
-        com.company.ListNode prev = null;
+        ListNode curr = head;
+        ListNode prev = null;
         
         while(curr != null){
             
-            com.company.ListNode tmp = curr.next;
+            ListNode tmp = curr.next;
             curr.next = prev;
             prev = curr;
             curr = tmp; 
