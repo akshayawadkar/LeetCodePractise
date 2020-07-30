76eb1a86b5f6791728276fedb2c85e3a36d894bc
akshayawadkar <mailaw3515@gmail.com>
2020-07-30 10:25:34 -0700

    added new

---
+++ _114.java
@@ -0,0 +1,38 @@
+package com.leetcode;
+
+public class _114 {
+
+    public void flatten(TreeNode root) {
+
+        if(root == null){
+            return;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+
+        while(curr != null || !stack.isEmpty()){
+
+
+            if(curr.right != null){
+                stack.push(curr.right);
+            }
+
+
+            if(curr.left != null){
+
+                curr.right = curr.left;
+                curr.left = null;
+
+            }else{
+                if(!stack.isEmpty()){
+                    curr.right = stack.pop();
+                }
+
+            }
+
+            curr = curr.right;
+        }
+
+    }
+}
---
+++ _116.java
@@ -0,0 +1,35 @@
+package com.leetcode;
+
+public class _116 {
+
+    public Node connect(Node root) {
+
+        if(root == null){
+            return root;
+        }
+
+        root.next = null;
+
+        dfs(root);
+
+        return root;
+    }
+
+    private void dfs(Node root){
+
+        if(root != null){
+
+            if(root.left != null){
+                root.left.next = root.right;
+            }
+
+            if(root.right != null){
+                root.right.next = root.next == null ? null : root.next.left;
+            }
+
+            dfs(root.left);
+            dfs(root.right);
+        }
+
+    }
+}
