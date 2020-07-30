c913da01d4aab78723d8bdbef0f1eb35e6f2034c
akshayawadkar <mailaw3515@gmail.com>
2020-07-30 10:52:19 -0700

    added new

---
+++ _117.java
@@ -0,0 +1,33 @@
+package com.leetcode;
+
+public class _117 {
+
+    public Node connect(Node root) {
+        helper(root);
+        return root;
+
+
+    }
+
+    private void helper(Node root){
+
+        Node dummyHead = new Node(0);
+        Node pre = dummyHead;
+        while (root != null) {
+            if (root.left != null) {
+                pre.next = root.left;
+                pre = pre.next;
+            }
+            if (root.right != null) {
+                pre.next = root.right;
+                pre = pre.next;
+            }
+            root = root.next;
+            if (root == null) {
+                pre = dummyHead;
+                root = dummyHead.next;
+                dummyHead.next = null;
+            }
+        }
+    }
+}
