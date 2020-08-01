97fa96b52e1fc3513129e6cd8a050e7b3c91e80e
akshayawadkar <mailaw3515@gmail.com>
2020-07-31 17:08:00 -0700

    added new

---
+++ _144.java
@@ -0,0 +1,33 @@
+package com.leetcode;
+
+public class _144 {
+    public List<Integer> preorderTraversal(TreeNode root) {
+
+        if(root == null){
+            return new ArrayList<>();
+        }
+
+        TreeNode curr = root;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(curr);
+
+        List<Integer> result = new ArrayList<>();
+
+        while(!stack.isEmpty()){
+
+            curr = stack.pop();
+            result.add(curr.val);
+
+            if(curr.right != null){
+                stack.push(curr.right);
+            }
+
+            if(curr.left != null){
+                stack.push(curr.left);
+            }
+
+        }
+
+        return result;
+    }
+}
