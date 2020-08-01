73271335d9820aab0479d515c5f6b7f3f9b5ce39
akshayawadkar <mailaw3515@gmail.com>
2020-07-31 17:29:48 -0700

    added new

--- _255.java
+++ _255.java
@@ -25,3 +25,25 @@ public class _255 {
         return true;
     }
 }
+
+/*
+* Consider this example
+* [10,8,6,9,12,11,13]
+*   this will return true;
+*
+* and if we change one value say we swap 9 with 6
+*   this will return false;
+*
+*   because 9 is found to be this first small number and if it happens that we come till 6
+*   it will compare 6 with 9 in this case 9 > 6 which is a wrong position
+*   :. it will retunr false;
+*
+*   but it not swapped we can see that
+*   6  would be the smallest no and nothing is greater than 6 in later traversal;
+*
+*
+*
+*
+* */
+
+
