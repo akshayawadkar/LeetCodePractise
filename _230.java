7101ac40682bee49d1a0b8b8dc96e3a073628619
akshayawadkar <mailaw3515@gmail.com>
2020-07-31 17:36:43 -0700

    added new

---
+++ _230.java
@@ -0,0 +1,31 @@
+package com.leetcode;
+
+public class _230 {
+
+    public int kthSmallest(TreeNode root, int k) {
+        if(root == null){
+            return 0;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+
+        while(!stack.isEmpty() || curr != null){
+
+            while(curr != null){
+                stack.push(curr);
+                curr = curr.left;
+            }
+
+            curr = stack.pop();
+            if(--k == 0){
+                return curr.val;
+            }
+            curr = curr.right;
+
+
+        }
+
+        return 0;
+    }
+}
