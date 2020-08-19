ad3c4e5df313824b27b1bfdf4aeb046d8b08a6b8
Akshay Anil Wadkar <akshayanilwadkar@Akshays-MBP.home>
2020-08-18 17:35:19 -0700

    new added

---
+++ _409.java
@@ -0,0 +1,32 @@
+public class _409 {
+
+        public int longestPalindrome(String s) {
+
+            Map<Character, Integer> map = new HashMap<>();
+            for(char ch : s.toCharArray()){
+                map.put(ch, map.getOrDefault(ch, 0) + 1);
+            }
+
+            int ans = 0;
+            boolean isFirstOdd = true;
+
+            for(Character ch : map.keySet()){
+                if(map.get(ch) % 2 == 0){
+                    ans += map.get(ch);
+                }else{
+                    if(isFirstOdd){
+                        isFirstOdd = false;
+                        ans += map.get(ch);
+                    }else{
+                        ans += map.get(ch) - 1;
+                    }
+
+                }
+            }
+
+
+
+            return ans;
+        }
+
+}
