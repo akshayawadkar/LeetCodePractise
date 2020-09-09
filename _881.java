284156f684ffe8897beda39311a85102dab1c68a
akshayawadkar <mailaw3515@gmail.com>
2020-09-08 18:42:25 -0700

    init

---
+++ _881.java
@@ -0,0 +1,27 @@
+public class _881 {
+    class Solution {
+        public int numRescueBoats(int[] people, int limit) {
+
+
+            Arrays.sort(people);
+
+            int result = 0;
+
+            int i = 0, j = people.length - 1;
+
+            while(i <= j){
+
+                if(people[i] + people[j] <= limit){
+                    i++; j--;
+
+                }else{
+                    j--;
+                }
+                result++;
+
+            }
+
+            return result;
+        }
+    }
+}
