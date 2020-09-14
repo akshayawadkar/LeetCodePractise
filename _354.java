4958ea3e8efe0e250be6a90f2dd27d437909c3f3
akshayawadkar <mailaw3515@gmail.com>
2020-09-14 14:41:17 -0700

    init

--- _354.java
+++ _354.java
@@ -1,4 +1,32 @@
 public class _354 {
+    /*
+
+A strict rule to be followed is
+when you are sorting on
+
+a[0][1] vs b[0][1]
+
+if(a[0] == b[0]){
+    return b[1] - a[1];
+}
+
+return a[0] - b[0];
+
+
+if a situation arises like [4,5][4,6]
+
+it should put [4,6] before [4,5] because
+
+not doing that will consider 2 envoploes
+
+but doing this will consider only one envolope
+
+
+Consider this example:
+[[4,5],[4,6],[6,7],[2,3],[1,1]]
+*/
+
+
     class Solution {
         public int maxEnvelopes(int[][] envelopes) {
             return solve(envelopes);
@@ -7,16 +35,16 @@ public class _354 {
         private int solve(int[][] arr) {
 
             Arrays.sort(arr, (a, b) -> {
-                if(a[0] == b[0]){
-                    return b[1] - a[1];
-                }
+                // if(a[0] == b[0]){
+                //     return b[1] - a[1];
+                // }
                 return a[0] - b[0];
             });
 
 
-//		for(int[] a : arr) {
-//			System.out.println(Arrays.toString(a));
-//		}
+            for(int[] a : arr) {
+                System.out.println(Arrays.toString(a));
+            }
 
             int n = arr.length;
             int[] dp = new int[n];
